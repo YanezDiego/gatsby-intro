82e5d57564bf788415718f338666f7e737893866
Diego Yanez <diegosyanez92@gmail.com>
2020-04-26 16:29:35 -0400

    finished

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby'
+import Layout from '../components/layout';
+
+export default () => (
+    <Layout>
+        <h1>About Me</h1>
+        <p>about me hello</p>
+        <Link to="/">&larr; take me home</Link>
+    </Layout>
+);
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,27 @@
 import React from 'react';
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import Hero  from '../components/hero'
+import PostPreview from '../components/post-preview'
+import usePosts from '../hooks/use-post'
 
-export default () => <div>Hello Frontend Masters!</div>;
+
+export default () => {
+    const posts = usePosts();
+    
+    
+    return (
+    <>
+        <Hero/>
+    <Layout>
+    <h1>Hello Frontend Masters!</h1>
+    <Link to="/about">About</Link>
+
+    <h2>Read My Blog</h2>
+        {posts.map( post => (
+            <PostPreview key={post.slug} post={post}/>
+        ))}
+
+    </Layout>
+    </>
+)}
